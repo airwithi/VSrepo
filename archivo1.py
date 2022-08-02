d5c32c30e2f2fb4cbd9a1bf9181b0c8d97dcc9ca
Esteban F <esteban.farfan.quevedo@gmail.com>
2022-08-02 13:13:30 -0500

    modificacion A rama master

--- archivo1.py
+++ archivo1.py
@@ -8,3 +8,6 @@ print("hola otra vez")
 print("modificacion para el cuarto commit")
 
 print("modificacion hecha unicamente en la rama master")
+
+
+print("modificacion A rama master para hacer merge")
