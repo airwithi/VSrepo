8168fbdcaf94b9353f38a508192bfc7eb91bda99
Esteban F <esteban.farfan.quevedo@gmail.com>
2022-08-02 13:46:46 -0500

    commit desde tercera rama gui

--- archivo1.py
+++ archivo1.py
@@ -10,3 +10,7 @@ print("modificacion para el cuarto commit")
 print("modificacion hecha unicamente en la rama master")
 
 print("modificacion hecha desde la tercera rama")
+
+
+
+print("modificacion hecha desde terceraRama para probar merge con interfaz grafica")
